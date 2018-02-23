dde69ddfca29e737038cfdf3fff9a0c587e086f8
tianzhaixing <tianzhaixing@126.com>
2018-02-23 15:22:47 +0800

    add noise function and update README.md
    Signed-off-by: tianzhaixing <tianzhaixing@126.com>

--- README.md
+++ README.md
@@ -23,8 +23,14 @@ Create folders recursively like the following structure in the root path of this
 
 ### Classes
 
-- 0 not phone
+- 0 not phone````````
 - 1 the feature phone
 - 2 the smart phone
 - 3 the back of phone
 - 4 lighten of phone screen
+
+
+## Reference
+
+- [python-logging](http://python.jobbole.com/86887/)
+- [python-opencv-noise](https://stackoverflow.com/questions/22937589/how-to-add-noise-gaussian-salt-and-pepper-etc-to-image-in-python-with-opencv)
--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 # -*- encoding:utf-8 -*-
 
 import os
+import cv2
+import sys
+import logging
 from PIL import Image
+from noisy import noisy
+
+# log
+## log instance
+logger = logging.getLogger("AddSamples")
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+
+## file_log
+file_handler = logging.FileHandler("addsamples.log")
+file_handler.setFormatter(formatter)
+
+## console_log
+console_hander = logging.StreamHandler(sys.stdout)
+console_hander.formatter = formatter
+
+logger.addHandler(file_handler)
+logger.addHandler(console_hander)
+
+## set log level default WARN
+logger.setLevel(logging.INFO)
+
+## remove file_log
+logger.removeHandler(file_handler)
+
+
+
+def add_noise(noise_type, save_prefix, img_prefix, image):
+    """
+    Add noise of noise_type to image.
+    :param noise_type: the type of noise
+    :param save_prefix: the prefix of save path
+    :param img_prefix: the prefix of image name
+    :param image: the cv type of image
+    :return:
+    """
+    noise_img = noisy(noise_type, image)
+    save_path = os.path.join(save_prefix, img_prefix + noise_type + ".jpg")
+    cv2.imwrite(save_path, noise_img)
 
 
 def preprocess_img(img_path, dst_sub_path, pfix):
@@ -11,13 +52,22 @@ def preprocess_img(img_path, dst_sub_path, pfix):
     if not os.path.isdir(dst_sub_path):
         print("The output path is invalid")
         return
+
+    # Rotate
     im = Image.open(img_path)
-    rotates = range(0, 361, 15)
+    rotates = range(0, 271, 90)
     for idx, v in enumerate(rotates):
+        img_prefix = str(pfix + idx)
         dst_im = im.rotate(v, 0, 1)
-        dst_file_path = os.path.join(dst_sub_path, str(pfix + idx) + ".jpg")
+        dst_file_path = os.path.join(dst_sub_path, img_prefix + "rotate.jpg")
         dst_im.save(dst_file_path)
-    return pfix + len(rotates)
+        # Add noise
+        cvimg = cv2.imread(dst_file_path)
+        add_noise("gauss", dst_sub_path, img_prefix, cvimg)
+        add_noise("poisson", dst_sub_path, img_prefix, cvimg)
+        # add_noise("s&p", dst_sub_path, img_prefix, cvimg)
+        # add_noise("speckle", dst_sub_path, img_prefix, cvimg)
+    return pfix + len(rotates) + 4
 
 
 def main(src_path, dst_path):
@@ -45,9 +95,10 @@ def main(src_path, dst_path):
                 continue
             os.mkdir(dst_sub)
             dst_subfolders.append(dst_sub)
-    print src_subfolders
-    print dst_subfolders
-    print '-' * 80
+
+    logger.info(src_subfolders)
+    logger.info(dst_subfolders)
+
     src_len = len(src_subfolders)
     dst_len = len(dst_subfolders)
     img_postfix = ['jpg', 'png', 'jpeg']
@@ -60,8 +111,9 @@ def main(src_path, dst_path):
                 if img[-3:] in img_postfix:
                     img_abs_path = os.path.join(src_sub, img)
                     pfix = preprocess_img(img_abs_path, dst_sub, pfix)
-            print('-' * 80)
-            print(src_sub.split("\\")[-1] + "Done!")
+            logger.info('-' * 80)
+            logger.info(src_sub.split("\\")[-1] + "\tDone!")
+            # break
 
 
 if __name__ == '__main__':
