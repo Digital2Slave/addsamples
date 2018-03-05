2fa4851a20d01518c5cb1ffe6fd10e21d1bb8a85
JohnTian <tianzhaixing@126.com>
2018-03-05 09:54:09 +0800

    change var to 49
    Signed-off-by: JohnTian <tianzhaixing@126.com>

--- noisy.py
+++ noisy.py
@@ -19,7 +19,7 @@ def noisy(noise_typ,image):
     if noise_typ == "gauss":
         row,col,ch= image.shape
         mean = 0
-        var = 0.1
+        var = 49
         sigma = var**0.5
         gauss = np.random.normal(mean,sigma,(row,col,ch))
         gauss = gauss.reshape(row,col,ch)
