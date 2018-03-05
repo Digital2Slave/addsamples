c358602dc984c837c56cfb425ae5888d83dfc3c9
tianzhaixing <tianzhaixing@126.com>
2018-03-05 14:11:27 +0800

    add crop function
    Signed-off-by: tianzhaixing <tianzhaixing@126.com>

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import os
 import cv2
 import sys
 import logging
+import numpy as np
 from PIL import Image
 from noisy import noisy
+from copy import deepcopy
 
 # log
 ## log instance
@@ -24,70 +26,98 @@ logger.addHandler(file_handler)
 logger.addHandler(console_hander)
 
 ## set log level default WARN
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 ## remove file_log
 logger.removeHandler(file_handler)
 
 
+def cropit(img_src_path, img_dst_path):
+    """
+    Crop image from src_img_path, and save it to dst_img_path.
+    :param img_src_path: the path of input image
+    :param img_dst_path: the path of output image
+    :return: None
+    """
+    pil_image = Image.open(img_src_path).convert('RGB')
+    src = np.array(pil_image)
+    src = src[:, :, ::-1].copy()
+    # src = cv2.imread(img_src_path)
+    img_info = np.shape(src)
+    gray = deepcopy(src)
+    if len(img_info) == 3:
+        cv2.cvtColor(src, cv2.COLOR_BGR2GRAY, gray)
+    gray = cv2.GaussianBlur(gray, (3,3), 0)
+    edges = cv2.Canny(gray, 30, 90, None, 3, False)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    mask = cv2.dilate(edges, kernel)
+    contours, hierarchy = cv2.findContours(mask, 0, 2)
 
-def add_noise(noise_type, save_prefix, img_prefix, image):
+    max_cnt = contours[0]
+    max_area = 0
+    for cnt in contours:
+        tmp_area = cv2.contourArea(cnt)
+        if tmp_area >= max_area:
+            max_area = tmp_area
+            max_cnt = cnt
+    if len(max_cnt):
+        x,y,w,h = cv2.boundingRect(max_cnt)
+        dst = src[y:y+h, x:x+w]
+        cv2.imwrite(img_dst_path, dst)
+    else:
+        cv2.imwrite(img_dst_path, src)
+
+
+def add_noise(noise_type, img_dst_dir, img_prefix, image):
     """
     Add noise of noise_type to image.
     :param noise_type: the type of noise
-    :param save_prefix: the prefix of save path
+    :param img_dst_dir: the prefix of save path
     :param img_prefix: the prefix of image name
     :param image: the cv type of image
     :return:
     """
     noise_img = noisy(noise_type, image)
-    save_path = os.path.join(save_prefix, img_prefix + noise_type + ".jpg")
+    save_path = os.path.join(img_dst_dir, img_prefix + "_noise_" + noise_type + ".jpg")
     cv2.imwrite(save_path, noise_img)
 
 
-def preprocess_img(img_path, dst_sub_path, pfix):
-    if not os.path.isfile(img_path):
-        print("The input path for image file is invalid.")
-        return
-    if not os.path.isdir(dst_sub_path):
-        print("The output path is invalid")
+def preprocess_img(img_src_path, img_dst_dir, img_prefix):
+    if not os.path.isfile(img_src_path):
+        logger.error("The input path for image file is invalid.")
         return
 
-    # Rotate
-    # im = Image.open(img_path)
-    # rotates = range(0, 271, 90)
-    # for idx, v in enumerate(rotates):
-    #     img_prefix = str(pfix + idx)
-    #     dst_im = im.rotate(v, 0, 1)
-    #     dst_file_path = os.path.join(dst_sub_path, img_prefix + "rotate.jpg")
-    #     dst_im.save(dst_file_path)
-    #     # Add noise
-    #     cvimg = cv2.imread(dst_file_path)
-    #     add_noise("gauss", dst_sub_path, img_prefix, cvimg)
-    #     # add_noise("poisson", dst_sub_path, img_prefix, cvimg)
-    #     # add_noise("s&p", dst_sub_path, img_prefix, cvimg)
-    #     # add_noise("speckle", dst_sub_path, img_prefix, cvimg)
-    # # return pfix + len(rotates)*2
+    # 0-Crop
+    img_dst_path = os.path.join(img_dst_dir, img_prefix + ".jpg")
+    cropit(img_src_path, img_dst_path)
 
-    # Only add noise
-    cvimg = cv2.imread(img_path)
-    img_prefix = str(pfix)
-    dst_file_path = os.path.join(dst_sub_path, img_prefix + ".jpg")
-    cv2.imwrite(dst_file_path, cvimg)
-    add_noise("gauss", dst_sub_path, img_prefix, cvimg)
-    add_noise("s&p", dst_sub_path, img_prefix, cvimg)
-    return pfix + 3
+    # 1-Rotate
+    im = Image.open(img_dst_path)
+    rotates = range(0, 271, 90)
+    for idx, v in enumerate(rotates):
+        dst_im = im.rotate(v, 0, 1)
+        img_pfix = img_prefix + "_rotate_" + str(v)
+        dst_file_path = os.path.join(img_dst_dir, img_pfix + ".jpg")
+        dst_im.save(dst_file_path)
+        # 2-Add Noise
+        cvimg = cv2.imread(dst_file_path)
+        add_noise("gauss", img_dst_dir, img_pfix, cvimg)
+        add_noise("s&p", img_dst_dir, img_pfix, cvimg)
 
 
 def main(src_path, dst_path):
     """
     main entry
+    :param src_path: the root folder of images from src_path
+    :param dst_path: the dst folder of images to dst_path
+    :return: None
     """
+
     if not os.path.isdir(src_path):
-        print("The input path is invalid.")
+        logger.error("The input path is invalid.")
         return
     if not os.path.isdir(dst_path):
-        print("The output path is invalid.")
+        logger.error("The output path is invalid.")
         return
 
     src_subfolders = []
@@ -105,8 +135,8 @@ def main(src_path, dst_path):
             os.mkdir(dst_sub)
             dst_subfolders.append(dst_sub)
 
-    logger.info(src_subfolders)
-    logger.info(dst_subfolders)
+    logger.debug(src_subfolders)
+    logger.debug(dst_subfolders)
 
     src_len = len(src_subfolders)
     dst_len = len(dst_subfolders)
@@ -115,13 +145,13 @@ def main(src_path, dst_path):
         for idn in range(src_len):
             src_sub = src_subfolders[idn]
             dst_sub = dst_subfolders[idn]
-            pfix = 0
             for img in os.listdir(src_sub):
                 if img[-3:] in img_postfix:
-                    img_abs_path = os.path.join(src_sub, img)
-                    pfix = preprocess_img(img_abs_path, dst_sub, pfix)
-            logger.info('-' * 80)
-            logger.info(src_sub.split("\\")[-1] + "\tDone!")
+                    img_prefix = img[:-4]
+                    img_src_path = os.path.join(src_sub, img)
+                    preprocess_img(img_src_path, dst_sub, img_prefix)
+            logger.debug('-' * 80)
+            logger.debug(src_sub.split("\\")[-1] + "\tDone!")
             # break
 
 
